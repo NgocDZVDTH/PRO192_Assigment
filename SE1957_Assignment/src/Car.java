b797bd300dee477da6099695263c7d45f5b620c0
vuducdo2108 <anhem2283@gmail.com>
2024-10-30 20:08:23 +0700

    Update Car.java

--- SE1957_Assignment/src/Car.java
+++ SE1957_Assignment/src/Car.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Car;
 
 import Brand.Brand;
-/**
- *
- * @author TUF F15
- */
+
 public class Car implements Comparable<Car>{
     private String carID;
     private Brand brand = new Brand();
