c91132f1c5e1aee50850c67894037b16683f2082
QC0106CC <he186215trinhminhquang@gmail.com>
2024-10-30 17:23:51 +0700

    Add files via upload

--- SE1957_Assignment/src/Car.java
+++ SE1957_Assignment/src/Car.java
@@ -2,62 +2,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package Car;
 
+import Brand.Brand;
 /**
  *
- * @author Hask
+ * @author TUF F15
  */
-public class Car implements Comparable<Car> {
-
+public class Car implements Comparable<Car>{
     private String carID;
-    private Brand brand;
+    private Brand brand = new Brand();
     private String color;
     private String frameID;
     private String engineID;
 
     public Car() {
-
     }
 
     public Car(String carID, Brand brand, String color, String frameID, String engineID) {
         this.carID = carID;
-        this.brand = brand;
         this.color = color;
         this.frameID = frameID;
         this.engineID = engineID;
+        this.brand = brand;
     }
 
-    // Getters and Setters for all fields
     public String getCarID() {
         return carID;
+    }
+
+    public void setCarID(String carID) {
+        this.carID = carID;
     }
 
     public Brand getBrand() {
         return brand;
     }
 
-    public String getColor() {
-        return color;
-    }
-
-    public String getFrameID() {
-        return frameID;
-    }
-
-    public String getEngineID() {
-        return engineID;
-    }
-
     public void setBrand(Brand brand) {
         this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
     }
 
     public void setColor(String color) {
         this.color = color;
     }
 
+    public String getFrameID() {
+        return frameID;
+    }
+
     public void setFrameID(String frameID) {
         this.frameID = frameID;
+    }
+
+    public String getEngineID() {
+        return engineID;
     }
 
     public void setEngineID(String engineID) {
@@ -65,16 +68,22 @@ public class Car implements Comparable<Car> {
     }
 
     @Override
-    public int compareTo(Car other) {
-        int brandComparison = this.brand.getBrandName().compareTo(other.brand.getBrandName());
-        if (brandComparison != 0) {
-            return brandComparison;
+    public int compareTo(Car car) {
+        int a = this.brand.getBrandName().compareTo(car.brand.getBrandName());
+        if (a != 0) {
+            return a;
+        } else {
+            return this.carID.compareTo(car.carID);
         }
-        return this.carID.compareTo(other.carID);
     }
 
-    // Method to return a string representation for displaying
+    @Override
+    public String toString() {
+        return this.carID + ", " + this.brand.getBrandID() + ", " + this.color + ", " + this.frameID + ", " + this.engineID;
+    }
+    
     public String screenString() {
-        return brand.getBrandName() + "\n" + carID + ", " + color + ", " + frameID + ", " + engineID;
+        return this.brand + "\n" + this.carID + ", " + this.color + ", " + this.frameID + ". " + this.engineID;
     }
 }
+
