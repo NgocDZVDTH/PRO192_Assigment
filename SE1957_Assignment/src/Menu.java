733a28c452a29877a9772eabeaa70a6a0efd876a
NgocDZVDTH <vu4488@gmail.com>
2024-11-03 21:09:02 +0700

    Update Menu.java

--- SE1957_Assignment/src/Menu.java
+++ SE1957_Assignment/src/Menu.java
@@ -1,13 +1,5 @@
 package Classes.Com;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author Hask
- */
 import java.util.Scanner;
 import java.util.ArrayList;
 
