e6ce89586bf1934e19dfa9fbcf39c545202a749a
Hask Bear <161166228+Haskbear@users.noreply.github.com>
2024-11-03 21:10:06 +0700

    Update BrandList.java

--- SE1957_Assignment/src/BrandList.java
+++ SE1957_Assignment/src/BrandList.java
@@ -8,14 +8,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author Hask
- */
 public class BrandList {
 
     private ArrayList<Brand> brands = new ArrayList<>();
